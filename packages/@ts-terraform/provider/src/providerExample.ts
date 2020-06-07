233f93751c8112ec574b1f2b6ab51c7c16041b76
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-06-07 11:50:00 +0100

    Add example files for debugging

---
+++ packages/@ts-terraform/provider/src/providerExample.ts
@@ -0,0 +1,37 @@
+import {createProvider} from './provider'
+
+async function run() {
+  const provider = await createProvider('./.terraform/plugins/darwin_amd64/terraform-provider-aws_v2.65.0_x4', {
+    debug: true,
+  })
+
+  await provider.configure({region: 'us-east-1'})
+
+  const importRes = await provider.importResourceState('aws_iam_user', 'jacob')
+  console.log(importRes)
+
+  const readRes = await provider.readResource('aws_iam_user', importRes[0].state)
+  console.log(readRes)
+
+  if (readRes) {
+    const planRes = await provider.planResourceChange(
+      'aws_iam_user',
+      readRes,
+      {...readRes, tags: {hello: 'world'}},
+      {private: importRes[0].private},
+    )
+    console.log(planRes)
+
+    const applyRes = await provider.applyResourceChange('aws_iam_user', readRes, planRes.plannedState, {
+      private: planRes.plannedPrivate,
+    })
+    console.log(applyRes)
+  }
+
+  await provider.shutdown()
+}
+
+run().catch((error: Error) => {
+  console.error(error.stack)
+  process.exit(1)
+})
